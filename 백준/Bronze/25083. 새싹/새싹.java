cf7fdc699d823724e20e94b71dc25268b67c5253
hayeoniill <lhy5441@naver.com>
2026-01-16 16:23:42 +0900

    [Bronze V] Title: 새싹, Time: 176 ms, Memory: 17468 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = "         ,r'\"7\n" + 
+                    "r`-_   ,'  ,/\n" + 		
+                     " \\. \". L_r'\n" + 		
+                    "   `~\\/\n" + 			
+                    "      |\n" + 			
+                     "      |";
+        
+        System.out.print(s);
+        sc.close();
+    }
+}
