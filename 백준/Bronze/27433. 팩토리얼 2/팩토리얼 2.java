26951c1612ca438284537267f799c11c732d46de
hayeoniill <lhy5441@naver.com>
2026-01-27 03:42:48 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+  public static long factorial(long n){
+    if(n==0 ||n==1)
+      return 1;
+    else{
+      return n * factorial(n-1);
+    }
+
+      
+  }
+    
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        long n = sc.nextLong();
+        System.out.println(factorial(n));
+       
+        sc.close();
+
+      
+      }
+
+      
+}
